46ebc02b9905dc5dc8f61a45cb21a1bd8a793cbf
Alex Gert <alex.gert1812@gmail.com>
2021-06-15 19:45:37 +0300

    15.06

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+
+export const App = () => {
+
+  return (
+      <>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus cupiditate doloremque ducimus, eaque eius excepturi facere fuga illum nobis non, nostrum officia officiis perspiciatis porro quia quis unde vero, voluptates.
+      </>
+  )
+}
+
+
